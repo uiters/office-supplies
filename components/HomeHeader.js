55df3573c185bb969977367aecced7911868499e
Phat Ngo <ntp18799@gmail.com>
2020-10-05 13:24:20 +0700

    add customized fonts and customized headers

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,31 +1,47 @@
-import * as React from "react";
+import React, {useState} from "react";
 import { ImageBackground, Text, View, StyleSheet } from "react-native";
 import DrawerButton from "./DrawerButton";
 import ShoppingCartButton from "./ShoppingCartButton";
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 
+
+const fetchFonts = () =>{
+  Font.loadAsync({
+    'SansitaSwashed-BlackItalic': require('../assets/fonts/SansitaSwashed-BlackItalic.ttf'),
+    'SansitaSwashed-MediumItalic':require('../assets/fonts/SansitaSwashed-MediumItalic.ttf')
+  });
+}
 const HomeHeader = ({ navigation }) => {
-  return (
-    <ImageBackground
-      source={{
-        uri:
-          "https://nongsansay.vn/wp-content/uploads/2020/04/5120x2880-light-green-solid-color-background-scaled.jpg",
-      }}
-      style={styles.ImageBackground}
-    >
-      <View style={styles.container}>
-        <View style={styles.subcontainer}>
-          <DrawerButton onOpenDrawer={() => navigation.openDrawer()} />
-          <Text style={styles.TextTitle}>Home</Text>
-          <ShoppingCartButton />
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if(!fontLoaded){
+    return <AppLoading 
+    startAsync={fetchFonts}
+    onFinish={()=>setFontLoaded(true)}/>;
+  }
+    return (
+      <ImageBackground
+        source={{
+          uri:
+            "https://nongsansay.vn/wp-content/uploads/2020/04/5120x2880-light-green-solid-color-background-scaled.jpg",
+        }}
+        style={styles.ImageBackground}
+      >
+        <View style={styles.container}>
+          <View style={styles.subcontainer}>
+            <DrawerButton onOpenDrawer={() => navigation.openDrawer()} />
+            <Text style={styles.TextTitle}>Home</Text>
+            <ShoppingCartButton />
+          </View>
         </View>
-      </View>
-    </ImageBackground>
-  );
+      </ImageBackground>
+    );
 };
 const styles = StyleSheet.create({
   ImageBackground: {
     width: "100%",
-    height: 150,
+    height: 100,
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
@@ -37,7 +53,7 @@ const styles = StyleSheet.create({
     elevation: 10,
   },
   container: {
-    height: 150,
+    height: 100,
     paddingTop: 50,
   },
   subcontainer: {
@@ -47,7 +63,8 @@ const styles = StyleSheet.create({
   },
   TextTitle: {
     color:"white",
-    fontSize: 23,
+    fontSize: 30,
+    fontFamily:'SansitaSwashed-BlackItalic',
   },
 });
 export default HomeHeader;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,24 +1,62 @@
-import * as React from "react";
-import { Button, StyleSheet, Text, View, ImageBackground } from "react-native";
-import DrawerButton from '../components/DrawerButton';
-import ShoppingCartButton from '../components/ShoppingCartButton';
-import NavigateToSearchPageButton from '../components/NavigateToSearchPageButton';
+import React, { useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+} from "react-native";
+import DrawerButton from "../components/DrawerButton";
+import ShoppingCartButton from "../components/ShoppingCartButton";
+import NavigateToSearchPageButton from "../components/NavigateToSearchPageButton";
 
 const HomeScreen = ({ route, navigation }) => {
+  const [text, setText] = useState("");
 
+  const onSubmitEditing = () => {
+    console.log(text);
+  };
   return (
-    <ImageBackground source={{
-      uri:
-        "https://nongsansay.vn/wp-content/uploads/2020/04/5120x2880-light-green-solid-color-background-scaled.jpg",
-    }}
-    style={styles.ImageBackground}>
-    </ImageBackground>
+    <View style={styles.container}>
+      <ImageBackground
+        source={{
+          uri:
+            "https://nongsansay.vn/wp-content/uploads/2020/04/5120x2880-light-green-solid-color-background-scaled.jpg",
+        }}
+        style={styles.ImageBackground}
+      >
+        <TextInput
+          style={styles.TextInput}
+          value={text}
+          onChangeText={(text) => setText(text)}
+          placeholder="What are you looking for?"
+          onSubmitEditing={onSubmitEditing}
+        />
+      </ImageBackground>
+    </View>
   );
 };
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   ImageBackground: {
+    height: 50,
     width: "100%",
-    height: "100%",
+    alignItems: "center",
+  },
+  TextInput: {
+    height: 50,
+    paddingLeft: 10,
+    width: "85%",
+    borderColor: "gray",
+    marginTop: 25,
+    borderWidth: 0.2,
+    backgroundColor: "white",
+    color: "black",
+    textShadowColor: 0.5,
+    borderRadius: 50,
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
@@ -26,7 +64,6 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.34,
     shadowRadius: 6.27,
-
     elevation: 10,
   },
 });
